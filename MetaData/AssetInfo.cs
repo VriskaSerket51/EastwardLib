a95b25d7f981d7b05b68a41538f02702c6fc9642
vriskaserket51 <serketvriska51@gmail.com>
2023-11-08 01:04:00 +0900

    Add groupType

--- MetaData/AssetInfo.cs
+++ MetaData/AssetInfo.cs
@@ -1,19 +1,22 @@
-﻿namespace EastwardLib.MetaData;
+﻿using SimpleJSON;
+
+namespace EastwardLib.MetaData;
 
 public struct AssetInfo
 {
     public string AssetName;
-    public string FilePath;
+    public string? FilePath;
     public string FileType;
+    public string? GroupType;
     public readonly Dictionary<string, string> ObjectFiles;
     public string Type;
 
-    public AssetInfo(string assetName, string filePath, string fileType, Dictionary<string, string> objectFiles,
-        string type)
+    public AssetInfo(string assetName, string? filePath, string fileType, string? groupType, Dictionary<string, string> objectFiles, string type)
     {
         AssetName = assetName;
         FilePath = filePath;
         FileType = fileType;
+        GroupType = groupType;
         ObjectFiles = objectFiles;
         Type = type;
     }
