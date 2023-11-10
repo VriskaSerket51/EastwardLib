7973fc9dfc5d3834b99a074b652d0bea47149d68
vriskaserket51 <serketvriska51@gmail.com>
2023-11-10 12:47:37 +0900

    feat: Add FileTree

---
+++ MetaData/FileTree.cs
@@ -0,0 +1,125 @@
+﻿using System.Text;
+
+namespace EastwardLib.MetaData;
+
+public class FileTree
+{
+    public struct Node
+    {
+        public enum Type
+        {
+            Directory,
+            File
+        }
+
+        public Type NodeType;
+        public string Name;
+        public Dictionary<string, Node>? Children;
+
+        public void AddChild(List<string> fileDirs)
+        {
+            Children ??= new Dictionary<string, Node>();
+
+            if (fileDirs.Count == 1)
+            {
+                Children.Add(fileDirs[0], new Node
+                {
+                    NodeType = Type.File,
+                    Name = fileDirs[0]
+                });
+                return;
+            }
+
+            var directory = fileDirs[0];
+            fileDirs.RemoveAt(0);
+
+            if (!Children.TryGetValue(directory, out var childNode))
+            {
+                childNode = new Node
+                {
+                    NodeType = Type.Directory,
+                    Name = directory
+                };
+            }
+
+            childNode.AddChild(fileDirs);
+
+            Children[directory] = childNode;
+        }
+    }
+
+    private Node _root = new()
+    {
+        NodeType = Node.Type.Directory,
+        Name = "root"
+    };
+
+    public void AppendFileName(string fileName)
+    {
+        _root.AddChild(fileName.Split('/').ToList());
+    }
+
+    public override string ToString()
+    {
+        return ToString(1);
+    }
+
+    public string ToString(int indent)
+    {
+        StringBuilder sb = new StringBuilder();
+        ToStringHelper(sb, indent, _root);
+        return sb.ToString();
+    }
+
+    private static void ToStringHelper(StringBuilder sb, int indent, Node node, int depth = 0, bool isLast = false,
+        int lastDepth = 0)
+    {
+        if (node.NodeType == Node.Type.Directory && node.Name == "IndieGrobert")
+        {
+            Console.WriteLine($"{depth}, {lastDepth}");
+        }
+
+        if (depth != 0)
+        {
+            for (int i = 0; i < lastDepth; i++)
+            {
+                sb.Append('│');
+                sb.Append(' ', indent);
+            }
+
+            sb.Append(' ', (depth - lastDepth - 1) * (indent + 1));
+
+            sb.Append(isLast ? '└' : '├');
+        }
+
+        switch (node.NodeType)
+        {
+            case Node.Type.Directory when node.Children == null || node.Children.Count == 0:
+                sb.Append("📁");
+                break;
+            case Node.Type.Directory:
+                sb.Append("📂");
+                break;
+            case Node.Type.File:
+                sb.Append("📄");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        sb.AppendLine(node.Name);
+
+        if (node.Children == null)
+        {
+            return;
+        }
+
+        int idx = 0;
+        foreach (var (_, childNode) in node.Children)
+        {
+            bool childIsLast = idx == node.Children.Count - 1;
+            ToStringHelper(sb, indent, childNode, depth + 1, childIsLast, isLast ? lastDepth : depth);
+            idx++;
+        }
+    }
+}
